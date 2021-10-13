0ee6281ea43ba70e3eb2d03e702a3b30b0731333
Cameron Taylor <cameron.taylor.ninja@gmail.com>
2021-10-13 18:46:37 -0400

    board stuff in progress

--- app.js
+++ app.js
@@ -2,12 +2,22 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+const bodyParser = require('body-parser');
+
 var mongoose = require("mongoose");
 const { json } = require('body-parser');
 mongoose.connect("mongodb://127.0.0.1:27017", { useNewUrlParser: true, useUnifiedTopology: true });
 
 
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
 const {Schema} = mongoose;
+
+const boardSchema = new Schema({
+    tasks: Array,
+    name: String
+});
 
 const todoSchema = new Schema({
     task: String,
@@ -16,24 +26,52 @@ const todoSchema = new Schema({
 
 const Todo = mongoose.model("Todo", todoSchema);
 
+const Board = mongoose.model("Board", boardSchema);
+
 app.get('/', (req, res) => 
 {
-    res.send("hello world!");
+    res.sendFile(__dirname + '/views/index.html');
 
 });
 
-app.get('/task', (req, res)=>{
-    Todo.create({task: "TEST SHIT", done: true}, function (err, todo)
+app.get("/boards/:boardname", (req, res)=>
+{
+    Board.findOne({name: req.params.boardname}, (err, board)=>
+    {
+        res.json(board);
+    });
+
+    // res.json()
+});
+
+app.post("/api/boardredirect", (req, res)=>
+{
+    Board.create({tasks: [], name: req.body.board}, (err, board)=>
+    {
+        if (err) return console.log(err);
+        
+        res.redirect("/boards/" + req.body.board);
+    });
+
+    
+});
+
+app.post("/api/task", (req, res) => 
+{
+
+    // res.json({body: req.body});
+    Todo.create({task: req.body.task, done: false}, function (err, todo)
     {
         if (err) return console.log(err);
 
+        res.json(todo);
+    });
+});
 
-        Todo.find({done: true}, function (err, todo)
-        {
-            res.json({ayay: todo});
-        });
-
-        
+app.get('/task', (req, res)=>{
+    Todo.find({done: false}, function (err, todo)
+    {
+        res.json({ayay: todo});
     });
 });
 
